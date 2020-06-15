2ef596abf1dad23fce139ee7ff3fa7d89a8d4b8f
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-15 18:37:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
       if (board[win_combo[0]] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X") || (board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O")
-        win_combo 
+       return win_combo 
       end
   end
   false
